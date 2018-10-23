3b3de1143a0b1810af8e8a7b685541fd3912d51f
unknown <pengliao2010@gmail.com>
2018-10-23 17:12:53 -0400

    added express middleware for api route

---
+++ src/server/index.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const compression = require('compression')
+
+const PORT = 8080
+const app = express()
+
+module.exports = app
+
+const createApp = () => {
+    // logging middleware
+    app.use(morgan('dev'))
+
+    // body parsing middleware
+    app.use(express.json())
+    app.use(express.urlencoded({extended: true}))
+
+    // compression middleware
+    app.use(compression())
+
+    // api route
+    app.use('/api', require('./api'))
+
+    // static file-serving middleware
+    app.use(express.static(path.join(__dirname, '../..', 'public')))
+
+    // handle 404
+    app.use((req, res, next) => {
+        if (path.extname(req.path).length){
+            const err = new Error('Not found')
+            err.status = 404
+            next(err)
+        } else {
+            next()
+        }
+    })
+
+    // error handle endware
+    app.use((err, req, res, next) => {
+        console.error(err)
+        console.error(err.stack)
+        res.status(err.status || 500).sent(err.message || 'Intenal server error.')
+    })
+}
+
+
+const startListening = () => {
+    app.listen(PORT, () => {
+        console.log(`Server start listening on port ${PORT}`)
+    })
+}
+
+async function bootApp() {
+    await createApp()
+    await startListening()
+  }
+
+if (require.main === module) {
+    bootApp()
+  } else {
+    createApp()
+  }
